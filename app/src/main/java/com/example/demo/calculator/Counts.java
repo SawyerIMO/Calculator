bbddedcf0879b307a12b3d2a7dd8a6ed89ca5a65
unknown <>
2017-05-11 14:13:59 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demo/calculator/Counts.java
@@ -0,0 +1,34 @@
+package com.example.demo.calculator;
+
+/**
+ * Created by 王嵩尧 on 2017/5/9.
+ */
+
+import java.math.BigDecimal;
+
+public enum Counts {
+    ADD, MINUS, MULTIPLY, DIVIDE, MARK, ROOT;
+    public String Values(String num1, String num2) {
+        BigDecimal number1 = new BigDecimal(num1);
+        BigDecimal number2 = new BigDecimal(num2);
+        BigDecimal number = BigDecimal.valueOf(0);
+        switch (this) {
+            case ADD:
+                number = number1.add(number2);
+                break;
+            case MINUS:
+                number = number1.subtract(number2);
+                break;
+            case MULTIPLY:
+                number = number1.multiply(number2);
+                break;
+            case DIVIDE:
+                number = number1.divide(number2,12,BigDecimal.ROUND_UP);
+                break;
+
+        }
+        return number.stripTrailingZeros().toString();
+
+    }
+
+}
